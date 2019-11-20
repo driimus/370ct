7355dbaa0c947c65be0061c2431ad65d13a1f5ee
carey <carey.pridgeon@gmail.com>
2019-11-20 14:00:49 +0000

    update

---
+++ openmp_examples/omp_for_with_params_and_sleep_example/README.md
@@ -0,0 +1,4 @@
+# 370CT_Exercises
+This incomplete example shows how to set up a paralel for loop to have
+perameters and a schedule type, as well as demonstrating how to use the system clock to delay threads.
+
---
+++ openmp_examples/omp_for_with_params_and_sleep_example/omp__for_with_params.cpp
@@ -0,0 +1,21 @@
+#include "omp.h"
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+#include <time.h>
+
+int main (void)
+{
+ int multiplier;
+ int list[16]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+ #pragma omp parallel for schedule (dynamic,2) private(multiplier)
+ for (int count=0; count<16;++count)
+     {
+
+      std::this_thread::sleep_for(std::chrono::seconds(2));
+     }
+ return 0;
+}
