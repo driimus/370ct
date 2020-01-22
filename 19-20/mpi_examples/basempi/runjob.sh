88cf33604a864311564f9cd6c6ff06ab1675e38f
carey <carey.pridgeon@gmail.com>
2020-01-22 12:39:40 +0000

    update

---
+++ 19-20/mpi_examples/basempi/runjob.sh
@@ -0,0 +1,9 @@
+#+BEGIN_SRC bash
+#!/bin/bash
+if [ "$1" == "" ]; then
+    echo "Positional parameter 1 is empty"
+else
+mpirun -machinefile /etc/pdsh/machines -np 11 --map-by ppr:4:node $1
+
+fi
+#+END_SRC
