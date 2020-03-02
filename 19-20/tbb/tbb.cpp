149756953fbfdb1c59b6997820bd2f849010c176
carey <carey.pridgeon@gmail.com>
2020-03-02 11:18:39 +0000

    update

--- 19-20/tbb/tbb.cpp
+++ 19-20/tbb/tbb.cpp
@@ -15,7 +15,7 @@ class ApplyFoo {
 public:
     void operator()( const blocked_range<size_t>& r ) const {
         float *a = my_a;
-        for( size_t i=r.begin(); i!=r.end(); ++i ) 
+        parallel_for( size_t i=r.begin(); i!=r.end(); ++i ) 
            Foo(a[i]);
     }
     ApplyFoo( float a[] ) :
