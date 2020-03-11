cd20048d34259b12f61d6ad96b257e6fee0d8551
carey <carey.pridgeon@gmail.com>
2020-03-11 17:32:38 +0000

    update

--- 19-20/mpi_examples/reduce/runJob.sh
+++ 19-20/mpi_examples/reduce/runJob.sh
@@ -3,6 +3,6 @@
 if [ "$1" == "" ]; then
     echo "Positional parameter 1 is empty"
 else
-    mpirun -machinefile /etc/pdsh/machines -np 11 --map-by ppr:4:node $1 
+mpirun -machinefile /etc/pdsh/machines -np 44 --map-by ppr:1:node $1 
 
 fi
