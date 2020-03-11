34a7ab589e626c35c84d402e3c5ef0849a7efb9b
carey <carey.pridgeon@gmail.com>
2020-03-11 11:53:07 +0000

    update

--- 19-20/mpi_examples/basic_send/main.cpp
+++ 19-20/mpi_examples/basic_send/main.cpp
@@ -13,14 +13,14 @@ int rank,size, namelen;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Get_processor_name(node_name, &namelen);
   memset(node_name+namelen,0,MPI_MAX_PROCESSOR_NAME-namelen);
-  int dest = 0;
+  int dest = 1;
         
   if (rank==0) {
     dest = (rand() % 10)+1;
     std::cout << "> destination: " << dest << std::endl;
     MPI_Send(&send_num, dest, MPI_INT, 1, 0, MPI_COMM_WORLD);
     std::cout << "> " <<node_name<<" sent " << send_num << "  to next node in line"<< std::endl;
-  } else if(rank == 1){
+  } else if(rank == dest){
      MPI_Recv(&received, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
      std::cout << "> " << received << " received by "<< node_name<< std::endl;
   }
