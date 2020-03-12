ac494c7533bbffafa5ebef73170507ecc3e6ba72
carey <carey.pridgeon@gmail.com>
2020-03-12 09:32:06 +0000

    update

--- 19-20/mpi_examples/reduce/runJob.sh
+++ 19-20/mpi_examples/reduce/runJob.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# This script can take multiple parameters
+# The first is the executable (required) any others would be passed as paremeter# to that executable
 
 if [ "$1" == "" ]; then
     echo "Positional parameter 1 is empty"
 else
-mpirun -machinefile /etc/pdsh/machines -np 44 --map-by ppr:4:node $1 
+mpirun -machinefile /etc/pdsh/machines -np 44 --map-by ppr:4:node $4 $2 $3
 
 fi
