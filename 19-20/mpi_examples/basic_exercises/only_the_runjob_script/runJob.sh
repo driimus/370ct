04c53ce5a5b19cbccd731441630096a90588b525
carey <carey.pridgeon@gmail.com>
2020-03-12 10:16:52 +0000

    update

--- 19-20/mpi_examples/basic_exercises/only_the_runjob_script/runJob.sh
+++ 19-20/mpi_examples/basic_exercises/only_the_runjob_script/runJob.sh
@@ -5,6 +5,6 @@
 if [ "$1" == "" ]; then
     echo "Positional parameter 1 is empty"
 else
-mpirun -machinefile /etc/pdsh/machines -np 44 --map-by ppr:4:node $4 $2 $3
+mpirun -machinefile /etc/pdsh/machines  --map-by ppr:4:node $4 $2 $3
 
 fi
