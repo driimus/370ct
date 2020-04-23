a15b86bd491a74ad66f7e8460b16b5e119b9a423
driimus <petrec@coventry.ac.uk>
2020-04-23 21:17:09 +0100

    Added last part tempalte

--- distributed/part_four.cpp
+++ distributed/part_four.cpp
@@ -10,12 +10,23 @@ auto getRandomInt(int from, int to) -> int {
 	return range(rnd);
 }
 
-// enum operations : int {
-// 	ADD = 0,
-// 	SUB,
-// 	mul,
-// 	div
-// }
+auto calculate(int num, int operator) -> int {
+	switch(operator) {
+		case 0:
+			num += getRandomInt(15, 60);
+			break;
+		case 1:
+			num -= getRandomInt(1, 15);
+			break;
+		case 2:
+			num *= getRandomInt(2, 9);
+			break;
+		case 3:
+			num /= getRandomInt(2, 5);
+			break;
+	}
+	return num;
+}
 
 auto main() -> int {
 	MPI_Init(NULL, NULL);
@@ -25,27 +36,34 @@ auto main() -> int {
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
 	std::vector<int> nodes;
-	for (int i = 0; i < world_size; ++i) nodes.push_back(i);
+	for (int i = 1; i < world_size; ++i) nodes.push_back(i);
 
-	int initial = getRandomInt(10, 99);
+	int data[3] = {
+		getRandomInt(10, 99), 				// initial number
+		getRandomInt(0, 3),						// initial arithmetic operator
+		getRandomInt(world_size, 15)	// amount of iterations
+	};
+
 	if (world_rank == 0) {
 		std::cout << "Initial: " << initial << std::endl;
 		int to = getRandomInt(1, world_size - 1);
 
-		MPI_Send(&initial, 1, MPI_INT, to, 0, MPI_COMM_WORLD);
-		MPI_Recv(&initial, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		std::cout << "Final result: " << initial << std::endl;
-	} else {
+		MPI_Send(&data, 3, MPI_INT, to, 0, MPI_COMM_WORLD);
+	}
+	else {
 		while(true) {
+			MPI_Status status;
+			MPI_Recv(&data, 3, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+			if (data[2] == 0) break;
+
+			data[0] = calculate(data[0], data[1]);
+			data[1] = (data[1]+1) % 4;
+			--data[2];
+
+			std::cout << "Node " << world_rank << ": " << data[0] << std::endl;
+
 			// Copy of node indices
 			std::vector<int> temp(nodes);
-
-			MPI_Status status;
-			MPI_Recv(&initial, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-			initial += 2;
-
-			std::cout << "Node " << world_rank << ": " << initial << std::endl;
-
 			// Flag previous and current nodes as visited.
 			int prev = status.MPI_SOURCE;
 			temp[prev] = prev == world_size-1
@@ -55,9 +73,18 @@ auto main() -> int {
 												? getRandomInt(0, world_size-2)
 												: world_rank + 1;
 
-			// Send to a random next node.
-			int to = getRandomInt(0, world_size - 1);
-			MPI_Send(&initial, 1, MPI_INT, temp[to], 0, MPI_COMM_WORLD);
+			if (data[2] == 0) {
+				// Finish up.
+				for (int i=1; i<world_size; ++i) {
+					if (i == world_rank) continue;
+					MPI_Send(&data, 3, MPI_INT, i, 0, MPI_COMM_WORLD);
+				}
+				break;
+			} else {
+				// Send to a random next node.
+				int to = getRandomInt(0, world_size - 1);
+				MPI_Send(&data, 3, MPI_INT, temp[to], 0, MPI_COMM_WORLD);
+			}
 		}
 	}
 
