dc9a777073765eea7610e137814b3a57a74fe754
driimus <petrec@coventry.ac.uk>
2020-04-23 21:46:10 +0100

    Added last part tempalte

--- distributed/part_four.cpp
+++ distributed/part_four.cpp
@@ -11,19 +11,19 @@ auto getRandomInt(int from, int to) -> int {
 }
 
 auto calculate(int num, int op) -> int {
-	switch(op) {
-		case 0:
-			num += getRandomInt(15, 60);
-			break;
-		case 1:
-			num -= getRandomInt(1, 15);
-			break;
-		case 2:
-			num *= getRandomInt(2, 9);
-			break;
-		case 3:
-			num /= getRandomInt(2, 5);
-			break;
+	switch (op) {
+	case 0:
+		num += getRandomInt(15, 60);
+		break;
+	case 1:
+		num -= getRandomInt(1, 15);
+		break;
+	case 2:
+		num *= getRandomInt(2, 9);
+		break;
+	case 3:
+		num /= getRandomInt(2, 5);
+		break;
 	}
 	return num;
 }
@@ -38,6 +38,7 @@ auto main() -> int {
 	std::vector<int> nodes;
 	for (int i = 1; i < world_size; ++i) nodes.push_back(i);
 
+	// Data that is passed between nodes.
 	int data[3] = {
 		getRandomInt(10, 99), 				// initial number
 		getRandomInt(0, 3),						// initial arithmetic operator
@@ -49,44 +50,43 @@ auto main() -> int {
 		int to = getRandomInt(1, world_size - 1);
 
 		MPI_Send(&data, 3, MPI_INT, to, 0, MPI_COMM_WORLD);
-	}
-	else {
-		while(true) {
+	} else {
+		while (true) {
 			MPI_Status status;
 			MPI_Recv(&data, 3, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-			if (data[2] == 0) break;
+			if (data[2] == 0) break; // If flagged for termination, exit loop.
 
 			data[0] = calculate(data[0], data[1]);
-			data[1] = (data[1]+1) % 4;
-			--data[2];
-
+			data[1] = (data[1] + 1) % 4;	// Pick another arithmetic operation
+			--data[2];	// Decrement remaining amount of iterations.
 
 			// Copy of node indices
 			std::vector<int> temp(nodes);
 			// Flag previous and current nodes as visited.
 			int prev = status.MPI_SOURCE;
-			if (prev) temp[prev-1] = prev == temp.size()
-														 ? getRandomInt(0, world_size-2)
-														 : prev + 1;
-			temp[world_rank-1] = world_rank == temp.size()
-												 ? getRandomInt(0, world_size-2)
-												 : world_rank + 1;
-			std::cout<< world_rank << temp[prev-1] << temp[world_rank-1] << '\n';
+			if (prev) temp[prev - 1] = prev == temp.size()
+				? getRandomInt(0, world_size - 2)
+				: prev + 1;
+			temp[world_rank - 1] = world_rank == temp.size()
+				? getRandomInt(0, world_size - 2)
+				: world_rank + 1;
 
+			// If enough iterations have passed.
 			if (data[2] == 0) {
-				// Finish up.
 				std::cout << "Final: " << data[0] << std::endl;
-				for (int i=1; i<world_size; ++i) {
+				// Notify other nodes to terminate.
+				for (int i = 1; i < world_size; ++i) {
 					if (i == world_rank) continue;
 					MPI_Send(&data, 3, MPI_INT, i, 0, MPI_COMM_WORLD);
 				}
+				// Terminate process on current node.
 				break;
 			} else {
 				// Send to a random next node.
 				int to = getRandomInt(0, world_size - 2);
 				std::cout << "Node " << world_rank << ": " << data[0]
-									<< ". From " << prev
-									<< ", passing to node " << temp[to] << std::endl;
+				          << ". From " << prev
+				          << ", passing to node " << temp[to] << std::endl;
 				MPI_Send(&data, 3, MPI_INT, temp[to], 0, MPI_COMM_WORLD);
 			}
 		}
